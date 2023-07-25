f73f2c0cbae01f9154479e1ed3e627befee1f6bd
khkim07 <jing607@gmail.com>
2023-07-25 10:09:12 +0900

    가변인자 함수 연습

---
+++ function3.py
@@ -0,0 +1,13 @@
+#function3.py
+#가변인자
+def union(*ar):
+    #지역변수(리스트)
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+print(union("HAM","SPAM"))
+print(union("HAM","SPAM","EGG"))
