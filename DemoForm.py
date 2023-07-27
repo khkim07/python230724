f39a5685d9f636671d6e15b337e9d6ee1c84e04e
khkim07 <jing607@gmail.com>
2023-07-27 15:23:14 +0900

    PyQt5 연습

--- DemoForm.py
+++ DemoForm.py
@@ -1,20 +1,17 @@
 #DemoForm.py
-import sys
-import typing
+import sys 
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5 import uic 
 
-#디자인된 문서 로딩
-form_lass = uic.loadUiType("DemoForm.ui")[0]
-#폼 클래스 정의
+#디자인 문서를 로딩
+form_class = uic.loadUiType("DemoForm.ui")[0]
+#윈도우 클래스 정의
 class DemoForm(QDialog, form_class):
-    #초기화메서드
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.label.setText("첫번째 화면")
-#직접 모듈을 실행했는지 체크(진입점)
+#모듈을 직접 실행했는지를 체크
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     demoForm = DemoForm()
